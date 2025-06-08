2fe141b196ae5011808eb1aa42319aba5b82976e
Igor Glamočak <igor.glamocak@scv.si>
2025-06-09 01:05:02 +0200

    Bids module, service, controller

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -11,17 +11,20 @@ import { configValidationSchema } from '../config/schema.config';
 import { UserModule } from './users/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AuctionModule } from './auctions/auction.module';
+import { BidModule } from './bids/bid/bid.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [`.env.${process.env.STAGE}`],
-      validationSchema: configValidationSchema,
-      UserModule,
-      AuthModule,
-      AuctionModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads/',
     }),
+    UserModule,
+    AuthModule,
+    AuctionModule,
+    BidModule,
   ],
   controllers: [],
   providers: [],
